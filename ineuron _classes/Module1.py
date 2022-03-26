1b29136dbe6192b0c0d30ced6a16a9594d1f2b3b
Nitin Chaudhary <57886482+nitin35byte@users.noreply.github.com>
2022-03-26 21:42:01 +0530

    Add files via upload

---
+++ ineuron _classes/Module1.py
@@ -0,0 +1,10 @@
+data ={"name":"ineuron",
+"course":["data Science","Block Chain","robotics","Cloud"],
+"greeting":"greeting fromm ineuron"
+}
+
+def get_courses():
+        return data["course"]
+
+def greeting():
+        return data["greeting"]
